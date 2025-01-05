38f9aa31e137251544d6d65f782715e847272804
MargowoN2705 <margowon2705@gmail.com>
2025-01-05 20:16:40 +0100

    Added upgrades for Beer - Male Life Ex

--- Analysys_Russia/Code/Bartek/Beer_Life_Ex_Plot.py
+++ Analysys_Russia/Code/Bartek/Beer_Life_Ex_Plot.py
@@ -6,6 +6,8 @@ temp_data.sort_values(by='Beer', ascending=True, inplace=True)
 temp_data = temp_data.dropna()
 y_1_values = temp_data['Beer']
 y_2_values = temp_data['males ']
+#y_2_values= y_2_values.fillna(y_2_values.mean())
+
 x_value = temp_data['Region']
 
 sns.set()
@@ -29,7 +31,7 @@ ax2.plot(x_value, regression_line_2, 'g--', label='Life Expectancy Regression')
 ax1.set_ylabel('Beer Consumption')
 ax2.set_ylabel('Life Expectancy)')
 ax1.set_xticks(range(len(x_value)))
-ax1.set_xticklabels(x_value, rotation=90)
+ax1.set_xticklabels(x_value, rotation=45, ha='right', fontsize=10)
 
 #ax1.set_ylim(30,70)
 ax2.set_ylim(55, 75)
@@ -38,5 +40,32 @@ fig.suptitle('Beer consumption and life expectancy of men in Russian regions')
 ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 fig.subplots_adjust(top=0.9, bottom=0.2)
+
+plt.show()
+
+plt.figure(figsize=(8, 6))
+
+coef = y_2_values / y_1_values
+print(coef)
+
+plt.scatter(x_value, coef, marker='o', color='blue', label='Male Life Ex/Beer')
+
+plt.xticks(rotation=45, ha='right', fontsize=12)
+
+plt.subplots_adjust(bottom=0.25, top=0.9)
+
+plt.tick_params(axis='x', pad=10)
+
+slope_3, intercept_3, _, _, _ = linregress(x_numeric, coef)
+regression_line_3 = slope_3 * x_numeric + intercept_3
+plt.plot(x_value, regression_line_3, '--', label='Coef Regression', color='purple')
+
+plt.title('Dependency of Beer Consumption vs. Male Life Expectancy', fontsize=14)
+plt.xlabel('Region', fontsize=12)
+plt.ylabel('Life Expectancy / Beer Consumption Ratio', fontsize=12)
+
+plt.ylim(min(coef) - 0.1, max(coef) + 0.1)
+
+plt.legend(loc='upper right')
 
 plt.show()
