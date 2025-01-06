e4dc9d6bc68028d8bb708a484031e17a942143a5
MargowoN2705 <margowon2705@gmail.com>
2025-01-06 01:44:13 +0100

    Update

--- Analysys_Russia/Code/Bartek/Main.py
+++ Analysys_Russia/Code/Bartek/Main.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import geopandas as gpd
 
 
 russian_data = pd.read_csv("../../Russia_22_data_updated (1).csv")
 consumptuon_data = pd.read_csv('../../Alkohol_Consumption.csv')
+geojson =  gpd.read_file("../../RussiaRegions.geojson")
+
 russian_data = russian_data.drop(columns=['Mapped_Region'])
